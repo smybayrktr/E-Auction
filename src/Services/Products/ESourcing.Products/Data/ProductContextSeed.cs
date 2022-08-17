d3b87b3a6e59ea10b6e340c55907ce9f95fa9bf9
SumeyyeBayraktar <smyyebyraktar@gmail.com>
2022-08-17 23:46:32 +0300

    Add project files.

---
+++ src/Services/Products/ESourcing.Products/Data/ProductContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using ESourcing.Products.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace ESourcing.Products.Data
+{
+    public class ProductContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetConfigureProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetConfigureProducts()
+        {
+            return new List<Product>() {
+
+                new Product{Name= "Telefon", Category="Elektronik", Description= "Temiz 2.el", Price= 22, Summary="daha ne diyem"},
+                new Product{Name= "Bilgisayar", Category="Elektronik", Description= "Temiz 2.el", Price= 24, Summary="daha ne diyem"},
+                new Product{Name= "TV", Category="Elektronik", Description= "Temiz 2.el", Price= 29, Summary="daha ne diyem"},
+                new Product{Name= "Soba", Category="Ev yaşam", Description= "Temiz 2.el", Price= 45, Summary="daha ne diyem" }
+            };
+        }
+    }
+}
---
+++ src/Services/Products/ESourcing.Products/Settings/ProductDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ESourcing.Products.Settings
+{
+    public class ProductDatabaseSettings : IProductDatabaseSettings
+    {
+        public string ConnectionStrings { set; get; }
+        public string DatabaseName { set; get; }
+        public string CollectionName { set; get; }
+    }
+}
