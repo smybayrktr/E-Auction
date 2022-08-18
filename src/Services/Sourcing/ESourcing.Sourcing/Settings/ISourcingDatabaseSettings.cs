8cc0489731798b012ad7974b049d37cb264d9ca0
SumeyyeBayraktar <smyyebyraktar@gmail.com>
2022-08-19 00:45:44 +0300

    Products finish and Sourcing Started

---
+++ src/Services/Sourcing/ESourcing.Sourcing/Settings/ISourcingDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ESourcing.Sourcing.Settings
+{
+    public interface ISourcingDatabaseSettings
+    {
+        public string ConnectionStrings { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
